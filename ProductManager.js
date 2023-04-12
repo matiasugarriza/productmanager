24272f3c328ba6852ae7dd37e3f2b4e5f1e40afa
Matias Ugarriza <matiasugarriza@gmail.com>
2023-04-11 21:11:11 -0300

    Desafio 1

--- ProductManager.js
+++ ProductManager.js
@@ -10,20 +10,44 @@ class ProductManager {
         this.stock = stock;
     }
 }
-//Funciones
+//Variables globales
 const products = [];
+let id = 0;
+//Funciones
 function getProducts() {
-    console.log(products);
+    console.log('Productos:', products);
+}
+function newId() {
+    if (products.find((product) => product.id !== 0)){
+       id++
+    }else{
+        id = 1
+    }
+    return id
 }
 
-function addProducts(id, title, description, price, thumbnail, code, stock){
-    
-    products.push(new ProductManager(id, title, description, price, thumbnail, code, stock))
-}
-function getProductById{
+function addProduct(id, title, description, price, thumbnail, code, stock) {
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.error('Error: todos los campos son obligatorios');
+    }else if (products.some((product) => product.code === code)) {
+        console.error('Error: el código de producto ya existe');
+    }else{
+        const product = new ProductManager(id, title, description, price, thumbnail, code, stock);
+        products.push(product);
+        console.log(product);
+    }
 
 }
-//Programa
-getProducts();
-addProducts('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);
-getProducts();
+
+function getProductById(id) {
+    let product = products.find((product) => product.id === id)
+    console.log(product ? product : "Not found")
+}
+
+//Testing de Entrega
+getProducts(); //Llamo al array que está vacío.
+addProduct(newId(),'producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);//Agrego un producto.
+getProducts();//Llamo neuvamente al array, esta vez tiene el producto agregado.
+addProduct(newId(),'producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);//Intento agregar nuevamente el mismo producto y da error.
+getProductById(1);//Busco el producto por su id y lo encuentra.
+getProductById(2);//Busco un producto que no existe y me da error.
