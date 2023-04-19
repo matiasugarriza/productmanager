19c1962a101a762b408a16e602cd149df6f7af85
Matias Ugarriza <matiasugarriza@gmail.com>
2023-04-18 22:26:44 -0300

    Desafio 1 v2

--- ProductManager.js
+++ ProductManager.js
@@ -1,49 +1,52 @@
 class ProductManager {
-    products = [];
-    id = 0;
-    constructor(id, title, description, price, thumbnail, code, stock){
-      this.id = id;
-      this.title = title;
-      this.description = description;
-      this.price = price;
-      this.thumbnail = thumbnail;
-      this.code = code;
-      this.stock = stock;
+    constructor(){
+      this.products = [];
+      this.id = 0;
     }
-    getProducts() {
-      return console.log('Productos:', this.products);
-    }
-    newId() {
-      if (this.products.find((product) => product.id !== 0)){
-        this.id++
-      } else {
-        this.id = 1
-      }
-      return this.id
-    }
-    addProduct(title, description, price, thumbnail, code, stock) {
-      const id = this.newId();
+    addProduct(title, description, price, thumbnail, code, stock){
+      // Validación para todos los campos obligatorios
       if (!title || !description || !price || !thumbnail || !code || !stock) {
-        console.error('Error: todos los campos son obligatorios');
-      } else if (this.products.some((product) => product.code === code)) {
-        console.error('Error: el código de producto ya existe');
-      } else {
-        const product = new ProductManager(id, title, description, price, thumbnail, code, stock);
-        this.products.push(product);
-        console.log(product);
+        console.error ('Error: Todos los campos son obligatorios.');
+        return;
       }
+      const existingProduct = this.products.find((product) => product.code === code)
+      //Validación 
+      if (existingProduct){
+        console.error (`Error: Ya existe un producto con el código ${code}`);
+        return;
+      }
+      //Id autoincremental
+      this.id++
+      //Se agrega un nuevo producto
+      const newProduct = {
+        id: this.id,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+      this.products.push(newProduct);
+      console.log(`Producto agregado con éxito. Nuevo Producto: ${JSON.stringify(newProduct)}`)
     }
-    getProductById(id) {
-      let product = this.products.find((product) => product.id === id)
-      console.log(product ? product : "Not found")
+    getProducts(){
+      return console.log(this.products);
     }
-  }
-  
+    getProductById(id){
+      const product = this.products.find((product) => product.id === id);
+      if (!product) {
+        console.error("Error: Producto no encontrado");
+        return;
+      }
+      return console.log(product);
+    }
+}
   //Testing de Entrega
-  const pm = new ProductManager();
-  pm.getProducts(); //Llamo al array que está vacío.
-  pm.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);//Agrego un producto.
-  pm.getProducts();//Llamo neuvamente al array, esta vez tiene el producto agregado.
-  pm.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);//Intento agregar nuevamente el mismo producto y da error.
-  pm.getProductById(1);//Busco el producto por su id y lo encuentra.
-  pm.getProductById(2);//Busco un producto que no existe y me da error.
+  const productManager = new ProductManager();
+  productManager.getProducts(); //Llamo al array que está vacío.
+  productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);//Agrego un producto.
+  productManager.getProducts();//Llamo neuvamente al array, esta vez tiene el producto agregado.
+  productManager.addProduct('producto prueba','Este es un producto prueba',200,'Sin imagen','abc123',25);//Intento agregar nuevamente el mismo producto y da error.
+  productManager.getProductById(1);//Busco el producto por su id y lo encuentra.
+  productManager.getProductById(2);//Busco un producto que no existe y me da error.
